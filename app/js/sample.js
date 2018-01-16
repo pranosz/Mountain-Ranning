d18a8479f62033dbee10fad92073c27dd857fb00
Piotr Ranosz <pranosz@wp.pl>
2018-01-16 20:32:41 +0100

    Multiselect component
    Added jQuery multiselect component to project.

--- app/js/sample.js
+++ app/js/sample.js
@@ -5,4 +5,27 @@
 	 $("#btn-more-filters").click(function(){
 	 	  $("#filters-advanced").toggle();
 	 });
-  } );
+
+   /*
+   * Multi select
+   */
+    $('#country').multiselect({
+        selectAll: true,
+        minHeight: null,
+        showCheckbox: true,
+        texts    : {
+            placeholder: 'Select Country',
+            search     : 'Search States'
+        }
+    });
+    $('#category').multiselect({
+        selectAll: true,
+        minHeight: null,
+        showCheckbox: true,
+        texts    : {
+            placeholder: 'Select Category',
+            search     : 'Search States'
+        }
+    });
+
+});
